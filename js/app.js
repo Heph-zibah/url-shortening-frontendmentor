f271fa3d6eb5ceb03f26449aa1197d5a0849d634
Heph-zibah <oluwatosinhephzibah@gmail.com>
2022-07-02 08:58:09 +0100

    update files

--- js/app.js
+++ js/app.js
@@ -2,10 +2,6 @@ const menuBar = document.querySelector('.menuBar');
 const closeBar = document.querySelector('.close--bar');
 const mobileMenu = document.querySelector('#mobile--modal');
 
-console.log(menuBar);
-console.log(closeBar);
-console.log(mobileMenu);
-
 menuBar.addEventListener('click', () => {
     closeBar.style.display = 'block';
     mobileMenu.style.display = 'block';
@@ -20,7 +16,7 @@ closeBar.addEventListener('click', () => {
 });
 
 window.addEventListener('click', (e) => {
-    if (e.target === modal) {
+    if (e.target === mobileMenu) {
         mobileMenu.style.display = 'none';
     }
 });
